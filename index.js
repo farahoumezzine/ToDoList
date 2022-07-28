2fe2e36e23cada4b3b7c3743434b34a500513cf7
Oumezzine Farah <oumezzine.farah@yahoo.com>
2022-07-28 15:46:28 +0100

    add function to test if it is the correct id

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const express=require("express")
 const fs =require ("fs")
@@ -37,7 +36,9 @@ fs.readFileSync('./list/todo.json','utf-8',(err,data)=>{
 
     }
     const todo=JSON.parse(data)
-    return response.json({todo : todo})
+    const IndexId = findTodoById(todo,id)
+
+    return response.json(todo[IndexId])
 })
 
 })
@@ -45,43 +46,4 @@ fs.readFileSync('./list/todo.json','utf-8',(err,data)=>{
 
 app.listen(3000,() => {
     console.log("working on http://localhost:3000");
-=======
-
-const express=require("express")
-const fs =require ("fs")
-
-const app =express()
-
-app.get('/', (request,response) =>{
-    return response.send("hello world")
-})
-app.get('/todo.json',(request,response) => {
-fs.readFile('./list/todo.json','utf-8',(err,data) =>{
-    if (err){
-        return response.status(500).send("Sorry, something went wrong.")
-    }
-        const todo =JSON.parse(data)
-        return response.json({todo : todo})
-})
-})
-
-app.put('/todo.json/:id/complete' , (request,response) =>{
-
-    const id =request.params.id
-
-fs.readFileSync('./list/todo.json','utf-8',(err,data)=>{
-    if (err){
-        return response.status(500).send("Sorry, something went wrong.")
-
-    }
-    const todo=JSON.parse(data)
-    return response.json({todo : todo})
-})
-
-})
-
-
-app.listen(3000,() => {
-    console.log("working on http://localhost:3000");
->>>>>>> f4c86a4b98d2df5b847fba66ddd295fc059ad268
 })
