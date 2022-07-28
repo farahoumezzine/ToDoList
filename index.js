66c76f3faac3272aa0664ac5bff5ceb803e43991
Oumezzine Farah <oumezzine.farah@yahoo.com>
2022-07-28 15:30:52 +0100

    add function find to-do by id

---
+++ index.js
@@ -0,0 +1,47 @@
+
+const express=require("express")
+const fs =require ("fs")
+
+const app =express()
+
+app.get('/', (request,response) =>{
+    return response.send("hello world")
+})
+app.get('/todo.json',(request,response) => {
+fs.readFile('./list/todo.json','utf-8',(err,data) =>{
+    if (err){
+        return response.status(500).send("Sorry, something went wrong.")
+    }
+        const todo =JSON.parse(data)
+        return response.json({todo : todo})
+})
+})
+
+app.put('/todo.json/:id/complete' , (request,response) =>{
+
+    const id =request.params.id
+
+    const findTodoById=(todo,id)=>{
+        for(let i=0; i<todo.length; i++){
+            if(todo[i].id===id){
+                return i
+            }
+        }
+        return -1 
+    }
+
+fs.readFileSync('./list/todo.json','utf-8',(err,data)=>{
+    if (err){
+        return response.status(500).send("Sorry, something went wrong.")
+
+    }
+    const todo=JSON.parse(data)
+    return response.json({todo : todo})
+})
+
+})
+
+
+app.listen(3000,() => {
+    console.log("working on http://localhost:3000");
+})
