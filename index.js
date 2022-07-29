e7d226bc599783ca06f5bd4efa9cbcda2323276d
Oumezzine Farah <oumezzine.farah@yahoo.com>
2022-07-29 16:00:01 +0100

    add items to the todo list

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 
+const { json, response } = require('express')
 const express=require('express')
 const fs =require ('fs')
+const { request } = require('http')
 
 const app =express()
+
+app.use(express.json())
+//built-in middleware function in Express. It parses incoming requests with urlencoded payloads and is based on body-parser.
+app.use(express.urlencoded({extended : true}))
 
 app.get('/', (request,response) =>{
     return response.send("Hello world")
@@ -64,6 +70,32 @@ fs.readFile('./list/todo.json','utf-8',(err,data)=>{
 
 })
 
+app.post('/todo',(request,response)=>{
+    if (!request.body.name){
+        return response.status(400).send('Missing name')
+    }
+    fs.readFile('./list/todo.json','utf-8',(err,data)=>{
+        if (err){
+            return response.status(500).send("Sorry, something went wrong.")
+    
+        }
+        const todo=JSON.parse(data)
+        const MaxId=Math.max.apply(Math,todo.map(a =>{ return a.id}))
+        todo.push({
+            id : MaxId+1,
+            name :request.body.name,
+            complete: false
+
+        })
+        fs.writeFile('./list/todo.json',JSON.stringify(todo) ,()=>{
+
+            return response.send({'status':'ok'})
+        }
+        )
+
+    })
+ 
+})
 
 app.listen(3000,() => {
     console.log("working on http://localhost:3000");
